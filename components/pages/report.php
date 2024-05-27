fc5d8ab4995ba2191568e89cd84a5487c41731ab
daryl <darylcerina38@gmail.com>
2024-05-27 19:51:34 +0800

    not yet but surely

--- components/pages/report.php
+++ components/pages/report.php
@@ -117,12 +117,16 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
-
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
-
-
     <script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.18/js/dataTables.bootstrap4.min.js"></script>
+    <!-- This is for creating pdf -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js"></script>
+    <!-- This is for editing an existing pdf -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf-lib/1.16.1/pdf-lib.min.js"></script>
+    
+
 
     <script>
         $(document).ready(function () {
